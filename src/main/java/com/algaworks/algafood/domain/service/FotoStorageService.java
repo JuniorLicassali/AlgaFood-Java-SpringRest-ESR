a9bde96b67d38eb0ed48a6b2cb20b8d0c9875980
JuniorLicassali <licassali88@gmail.com>
2023-12-12 12:23:29 -0300

    Uploading images

---
+++ src/main/java/com/algaworks/algafood/domain/service/FotoStorageService.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood.domain.service;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public interface FotoStorageService {
+	
+	InputStream recuperar(String nomeArquivo);
+
+	void armazenar(NovaFoto novaFoto);
+	
+	void remover(String nomeArquivo);
+	
+	default void substituir(String nomeArquivoAntigo, NovaFoto novaFoto) {
+		this.armazenar(novaFoto);
+		
+		if (nomeArquivoAntigo != null) {
+			this.remover(nomeArquivoAntigo);
+		}
+	}
+	
+	default String gerarNomeArquivo(String nomeOriginal) {
+		return UUID.randomUUID().toString() + "_" + nomeOriginal;
+	}
+	
+	@Builder
+	@Getter
+	class NovaFoto {
+		private String nomeArquivo;
+		private InputStream inputStream;
+	}
+}
